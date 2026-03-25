af1963cbd54889cd28a6122771679a97024957c5
chaymakharrat <chaimakharrat6@gmail.com>
2026-03-25 02:31:18 +0100

    fix logging config departement-service

---
+++ notification-service/src/main/java/pfe/example/notificationservice/entities/NotificationInterne.java
@@ -0,0 +1,4 @@
+package pfe.example.notificationservice.entities;
+
+public class NotificationInterne {
+}
---
+++ service-workflow/src/main/java/com/example/workflow/delegate/completeGroupe/PromouvoirCandidatsDelegate.java
@@ -0,0 +1,4 @@
+package com.example.workflow.delegate.completeGroupe;
+
+public class PromouvoirCandidatsDelegate {
+}
